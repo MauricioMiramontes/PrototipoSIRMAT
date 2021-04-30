7008bce128f5b3a4e2867bcdaf8e7b2b4cfef453
eduardordl97 <erdl5259@gmail.com>
2021-04-29 20:47:54 -0500

    Añadiendo view de Trampas

--- SIRMAT/PrototipoSIRMAT/urls.py
+++ SIRMAT/PrototipoSIRMAT/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('trampas/', include('Trampas.urls')),
     path('',include('Home.urls')),
 ]
--- SIRMAT/Trampas/views.py
+++ SIRMAT/Trampas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from Trampas.models import Trampas
 # Create your views here.
+
+
+def trampas(request):
+
+    trampas=Trampas.objects.all()
+    return render(request, 'Trampas/trampas.html',{'trampas':trampas})
