c75bb9cfea0f9d149312525884bcb04356ef5f15
eduardordl97 <erdl5259@gmail.com>
2021-07-14 13:27:10 -0500

    Configuracion de CORS en el backend

--- SIRMAT/API_SIRMAT/settings.py
+++ SIRMAT/API_SIRMAT/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     # Apps rest_framework
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
     # App Documentacion API
     'drf_yasg',
@@ -87,6 +88,7 @@ SWAGGER_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -165,6 +167,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -177,3 +181,6 @@ STATICFILES_DIRS = (
 )
 
 AUTH_USER_MODEL = 'Usuario.User'
+
+
+
