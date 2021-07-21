d6d4386117627a3727544015cd0c8f5fbcb68dcb
MauricioMiramontes <mmiramontesr@gmail.com>
2021-07-20 20:05:22 -0500

    Se sube prototipo de DELETE usando fetch

--- SIRMAT/frontend/src/views/Camaras.js
+++ SIRMAT/frontend/src/views/Camaras.js
@@ -89,174 +89,191 @@ class TablaCamaras extends Component {
   };
 
   //Funcion que se utilizara para hacer DELETE a la API en Camaras
-  DELETE_camaras(id){
+  DELETE_camaras(id) {
+    
+    // Se de la formato a el parametro id
+    var params = {"id" : id};
+
+    // Se crea la URL para mandar a la API
+    const url = "http://127.0.0.1:8081/camaras/?" + new URLSearchParams(params);
+
+    // Se esconde el mensaje de confirmacion para eliminar
     this.setState({ delete_dialog: false })
-    console.log('Se va a borrar')
-    console.log(id)
+
+    //Se hace la llamada a la API
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        //Cambiar token dependiendo a quien este manipulando las pruebas
+        'Authorization': 'Token ' + this.state.user_data.token,
+      },
+    })
+    .then(response => response.json())
+    .then(respuesta_delete => console.log(respuesta_delete) )
+};
+
+// Funcion que crea la tabla con los datos que se hayan recolectado de la API
+create_table = (camaras) => {
+
+
+  //Dependiendo del valor que tenga is_active se mostrara un valor distinto en "Estado"
+  const print_is_active = (is_active) => {
+    if (is_active === true) {
+      return (
+        <Badge color="" className="badge-dot">
+          <i className="bg-success" />
+          Activa
+        </Badge>
+      )
+    }
+    else if (is_active === false) {
+      return (
+        <Badge color="" className="badge-dot">
+          <i className="bg-danger" />
+          Dado de baja
+        </Badge>
+      )
+    };
   };
 
-  // Funcion que crea la tabla con los datos que se hayan recolectado de la API
-  create_table = (camaras) => {
-
-
-    //Dependiendo del valor que tenga is_active se mostrara un valor distinto en "Estado"
-    const print_is_active = (is_active) => {
-      if (is_active === true) {
-        return (
-          <Badge color="" className="badge-dot">
-            <i className="bg-success" />
-            Activa
-          </Badge>
-        )
-      }
-      else if (is_active === false) {
-        return (
-          <Badge color="" className="badge-dot">
-            <i className="bg-danger" />
-            Dado de baja
-          </Badge>
-        )
-      };
-    };
-
-    // Se regresa el contenido de la tabla con los datos de cada uno 
-    // De los registros que contenga la lista "camaras" usando una funcion map()
-    return camaras.map((camara) => {
-      return (
-        <tr>
-          <th scope="row">{camara.marca}</th>
-          <td>{camara.foco}</td>
-          <td>{camara.resolucion}</td>
-          <td>{print_is_active(camara.is_active)}</td>
-          <td className="text-right">
-            <UncontrolledDropdown>
-              <DropdownToggle
-                className="btn-icon-only text-light"
+  // Se regresa el contenido de la tabla con los datos de cada uno 
+  // De los registros que contenga la lista "camaras" usando una funcion map()
+  return camaras.map((camara) => {
+    return (
+      <tr>
+        <th scope="row">{camara.marca}</th>
+        <td>{camara.foco}</td>
+        <td>{camara.resolucion}</td>
+        <td>{print_is_active(camara.is_active)}</td>
+        <td className="text-right">
+          <UncontrolledDropdown>
+            <DropdownToggle
+              className="btn-icon-only text-light"
+              href="#pablo"
+              role="button"
+              size="m"
+              color=""
+              onClick={(e) => e.preventDefault()}
+            >
+              <i className="fas fa-ellipsis-v" />
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" container="body" right>
+              <DropdownItem
                 href="#pablo"
-                role="button"
-                size="m"
-                color=""
                 onClick={(e) => e.preventDefault()}
               >
-                <i className="fas fa-ellipsis-v" />
-              </DropdownToggle>
-              <DropdownMenu className="dropdown-menu-arrow" container="body" right>
-                <DropdownItem
-                  href="#pablo"
-                  onClick={(e) => e.preventDefault()}
-                >
-                  Editar
-                </DropdownItem>
-                <DropdownItem
-                  href="#pablo"
-                  onClick={() => this.setState({ delete_dialog: true, camara_seleccionada: camara.idcCamaras })}
-                >
-                  Dar de baja
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </td>
-        </tr>
-      );
-    });
-  };
-
-  render() {
-
-    return (
-      <>
-        <Header />
-        <ConfirmDelete
-          isOpen={this.state.delete_dialog}
-          onClose={(e) => this.setState({ delete_dialog: false })}
-          onConfirm={() => this.DELETE_camaras(this.state.camara_seleccionada)} />
-        <Container className="mt--7" fluid>
-          {/* Tabla */}
-          <Row>
-            <div className="col">
-              <Card className="shadow">
-                <CardHeader className="border-0">
-                  <Row className="align-items-center">
-                    <h3 className="mb-0 ml-2">Camaras</h3>
-                    <Button className="ml-3" color="success" type="button" size="sm">
-                      <i className="ni ni-fat-add mt-1"></i>
-                    </Button>
-                  </Row>
-                </CardHeader>
-                <Table className="align-items-center table-flush" responsive>
-                  <thead className="thead-light">
-                    <tr>
-                      <th scope="col">Marca</th>
-                      <th scope="col">Foco</th>
-                      <th scope="col">Resolucion</th>
-                      <th scope="col">Estado</th>
-                      <th scope="col" />
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {/* Se llama a la funcion que crea la tabla */}
-                    {this.create_table(this.state.table_data)}
-                  </tbody>
-                </Table>
-                <CardFooter className="py-4">
-                  <nav aria-label="...">
-                    <Pagination
-                      className="pagination justify-content-end mb-0"
-                      listClassName="justify-content-end mb-0"
-                    >
-                      <PaginationItem className="disabled">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                          tabIndex="-1"
-                        >
-                          <i className="fas fa-angle-left" />
-                          <span className="sr-only">Previous</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem className="active">
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          1
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          2 <span className="sr-only">(current)</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          3
-                        </PaginationLink>
-                      </PaginationItem>
-                      <PaginationItem>
-                        <PaginationLink
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <i className="fas fa-angle-right" />
-                          <span className="sr-only">Next</span>
-                        </PaginationLink>
-                      </PaginationItem>
-                    </Pagination>
-                  </nav>
-                </CardFooter>
-              </Card>
-            </div>
-          </Row>
-        </Container>
-      </>
+                Editar
+              </DropdownItem>
+              <DropdownItem
+                href="#pablo"
+                onClick={() => this.setState({ delete_dialog: true, camara_seleccionada: camara.idcCamaras })}
+              >
+                Dar de baja
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </td>
+      </tr>
     );
-  }
+  });
+};
+
+render() {
+
+  return (
+    <>
+      <Header />
+      <ConfirmDelete
+        isOpen={this.state.delete_dialog}
+        onClose={(e) => this.setState({ delete_dialog: false })}
+        onConfirm={() => this.DELETE_camaras(this.state.camara_seleccionada)} />
+      <Container className="mt--7" fluid>
+        {/* Tabla */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <h3 className="mb-0 ml-2">Camaras</h3>
+                  <Button className="ml-3" color="success" type="button" size="sm">
+                    <i className="ni ni-fat-add mt-1"></i>
+                  </Button>
+                </Row>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Marca</th>
+                    <th scope="col">Foco</th>
+                    <th scope="col">Resolucion</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col" />
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* Se llama a la funcion que crea la tabla */}
+                  {this.create_table(this.state.table_data)}
+                </tbody>
+              </Table>
+              <CardFooter className="py-4">
+                <nav aria-label="...">
+                  <Pagination
+                    className="pagination justify-content-end mb-0"
+                    listClassName="justify-content-end mb-0"
+                  >
+                    <PaginationItem className="disabled">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                        tabIndex="-1"
+                      >
+                        <i className="fas fa-angle-left" />
+                        <span className="sr-only">Previous</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem className="active">
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        1
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        2 <span className="sr-only">(current)</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        3
+                      </PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <PaginationLink
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        <i className="fas fa-angle-right" />
+                        <span className="sr-only">Next</span>
+                      </PaginationLink>
+                    </PaginationItem>
+                  </Pagination>
+                </nav>
+              </CardFooter>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+}
 };
 
 export default TablaCamaras;
