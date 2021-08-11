6b2b7d60a8c7b6e3aa65d6ee1a0a9f890340a332
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-08-11 17:41:34 -0500

    Actualizado models.py de DetallesMuestra #57
    Co-Authored-By: Ulises-Portuguez <84081256+Ulises-Portuguez@users.noreply.github.com>
    Co-Authored-By: LuisManuelRosalesGutierrez <80172295+LuisManuelRosalesGutierrez@users.noreply.github.com>

--- SIRMAT/DetallesMuestra/models.py
+++ SIRMAT/DetallesMuestra/models.py
@@ -9,23 +9,21 @@ class DetallesMuestra(models.Model):
     idtDetallesMuestra = models.BigAutoField(
         primary_key=True, help_text='Numero de identificacion del registro')
 
-    idEspecie = models.ForeignKey(
-        Especie, on_delete=models.CASCADE, help_text='Especies encontradas en la muestra')
-    
     horaFecha = models.DateTimeField(
         auto_now_add=True, help_text='Hora en la que se registro la Muestra')
 
     observaciones = models.CharField(
         max_length=45, help_text='Observaciones del empleado sobre la muestra')
     
-    cantidad = models.IntegerField(
-        help_text='Cantidad de insectos detectados en la muestra')
-    
     idMuestra = models.OneToOneField(
         Muestra, on_delete=models.CASCADE, help_text='Muestra a la que pertenecen estos detalles')
     
     is_active = models.BooleanField(
         default=True, help_text='Define si el registro esta o no dado de baja en el sistema')
+
+    Especies = models.JSONField(
+        help_text='Indica cuales especies se observo en la muestra y sus cantidades',
+        default = dict)
 
     class Meta:
         db_table = 'tDetallesMuestra'
