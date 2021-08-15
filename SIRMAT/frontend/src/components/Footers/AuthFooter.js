55e0ff596024d9e446b5139493e5318cc5cd86b1
eduardordl97 <erdl5259@gmail.com>
2021-08-15 14:00:31 -0500

    Cambio a Footer y Cambio a Iconos #29

--- SIRMAT/frontend/src/components/Footers/AuthFooter.js
+++ SIRMAT/frontend/src/components/Footers/AuthFooter.js
@@ -35,42 +35,19 @@ const Login = () => {
                   href="https://www.creative-tim.com?ref=adr-auth-footer"
                   target="_blank"
                 >
-                  Creative Tim
-                </a>
+                  SIRMAT
+                </a> ALL Rights Reserved
               </div>
             </Col>
             <Col xl="6">
               <Nav className="nav-footer justify-content-center justify-content-xl-end">
+               
                 <NavItem>
                   <NavLink
-                    href="https://www.creative-tim.com?ref=adr-auth-footer"
+                    href="#"
                     target="_blank"
                   >
-                    Creative Tim
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://www.creative-tim.com/presentation?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    About Us
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="http://blog.creative-tim.com?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    Blog
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-auth-footer"
-                    target="_blank"
-                  >
-                    MIT License
+                    Contact
                   </NavLink>
                 </NavItem>
               </Nav>
