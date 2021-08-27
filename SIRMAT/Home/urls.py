6e61ab34eafcb1fcf3a978980ccdb82b1fb11c62
Mauricio Miramontes Ramirez <mauriciomiramontes58@aragon.unam.mx>
2021-08-26 20:34:27 -0500

    Se corrige error #58

--- SIRMAT/Home/urls.py
+++ SIRMAT/Home/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import re_path
 from Home import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', views.home, name='Home'),  
+    re_path('',views.home, name='Home'),
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
